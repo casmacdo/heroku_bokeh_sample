ca9ac70910a0cfdc521df23812a1b09b594a8dde
Cassidy MacDonald <catricks@protonmail.com>
2022-09-21 18:45:30 -0400

    fixed the app

--- myapp.py
+++ myapp.py
@@ -1,13 +1,60 @@
-from bokeh.plotting import figure, show, output_file, curdoc
-from bokeh.tile_providers import get_provider, Vendors
 
-output_file("tile.html")
+import numpy as np
 
-tile_provider = get_provider(Vendors.ESRI_IMAGERY)
+from bokeh.io import curdoc
+from bokeh.layouts import column, row
+from bokeh.models import ColumnDataSource, Slider, TextInput
+from bokeh.plotting import figure
 
-# range bounds supplied in web mercator coordinates
-p = figure(x_range=(-8184831.46,-8190000.00 ), y_range=(5701314.14, 5710000.00),
-           x_axis_type="mercator", y_axis_type="mercator")
-p.add_tile(tile_provider)
+# Set up data
+N = 200
+x = np.linspace(0, 4*np.pi, N)
+y = np.sin(x)
+source = ColumnDataSource(data=dict(x=x, y=y))
 
-curdoc().add_root(p)
+
+# Set up plot
+plot = figure(height=400, width=400, title="my sine wave",
+              tools="crosshair,pan,reset,save,wheel_zoom",
+              x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
+
+plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
+
+
+# Set up widgets
+text = TextInput(title="title", value='my sine wave')
+offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
+amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
+phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
+freq = Slider(title="frequency", value=1.0, start=0.1, end=5.1, step=0.1)
+
+
+# Set up callbacks
+def update_title(attrname, old, new):
+    plot.title.text = text.value
+
+text.on_change('value', update_title)
+
+def update_data(attrname, old, new):
+
+    # Get the current slider values
+    a = amplitude.value
+    b = offset.value
+    w = phase.value
+    k = freq.value
+
+    # Generate the new curve
+    x = np.linspace(0, 4*np.pi, N)
+    y = a*np.sin(k*x + w) + b
+
+    source.data = dict(x=x, y=y)
+
+for w in [offset, amplitude, phase, freq]:
+    w.on_change('value', update_data)
+
+
+# Set up layouts and add to document
+inputs = column(text, offset, amplitude, phase, freq)
+
+curdoc().add_root(row(inputs, plot, width=800))
+curdoc().title = "Sliders"
